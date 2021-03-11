53778838d0a280185440485fd100b605572b570d
Yosif Georgiev <iodesign@live.com>
2021-03-11 20:14:30 +0200

    10th

--- next.config.js
+++ next.config.js
@@ -18,7 +18,6 @@ module.exports = {
         deviceSizes: [320, 420, 768, 1024, 1200],
         iconSizes: [],
         domains: ["https://res.cloudinary.com/"],
-        path: '/_next/image',
-        loader: 'cloudinary',   
+        loader: 'cloudinary'  
       }
 }
